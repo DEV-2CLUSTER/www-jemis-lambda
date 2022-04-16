736fa0edad78552b675ccefb5c55577c385f81a4
dev@2cluster.com <dev@2cluster.com>
2022-04-17 01:26:50 +0200

    test6

--- index.ts
+++ index.ts
@@ -1,18 +1,38 @@
+import * as AWS from "aws-sdk";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResultV2,
   Handler,
 } from "aws-lambda";
+import * as mysql from "mysql";
 import * as _ from "lodash";
+
+AWS.config.update({ region: "eu-west-3" });
+
+const host = "dftudbstrevdtgf.cps2q1ntjqzp.eu-west-3.rds.amazonaws.com";
+const user = "admin";
+const password = "jnP03N1NTgpr";
+const database = "jemis";
+
+const connect = mysql.createConnection({
+  ssl: { rejectUnauthorized: false },
+  host,
+  user,
+  password,
+  database,
+});
 
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResultV2> => {
-  const max = 10;
-  const val = _.random(max);
   const response = {
     statusCode: 200,
-    body: JSON.stringify(`numbertje: ${val}`),
+    body: JSON.stringify("error"),
   };
+  connect.query("SELECT 1", function (error, results, fields) {
+    if (error) throw error;
+    response.body = results;
+  });
+
   return response;
 };
